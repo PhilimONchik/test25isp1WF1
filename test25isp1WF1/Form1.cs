028f16b4d5b7313fba3664ee5c2f78852b380659
phili <phili@PHILIMONCHIK>
2026-09-22 08:48:47 +0500

    Add project files.

---
+++ test25isp1WF1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test25isp1WF1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ёёёёё, всем приывет!");
+            label1.Text = "Емаё как есело т о аааааа";
+        }
+    }
+}
